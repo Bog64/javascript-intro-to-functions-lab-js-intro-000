9b67d14a471188b9743e2804540c5e39c8806a0f
Ben Gorenstein <bengorenstein@gmail.com>
2021-01-14 18:24:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ var lowercase = "hello"
 var p = "I love you, Grandma"
 function sayHiToGrandma(string){
   if (string = lowercase) {
-    return 'I can\'t hear you!'
+    return 'I can\'t hear you!';
 }else if (string = uppercase){
-  return "YES INDEED!"
+  return "YES INDEED!";
 }else if (p){
-  return "I love you, too."
+  return "I love you, too.";
 }
 }
