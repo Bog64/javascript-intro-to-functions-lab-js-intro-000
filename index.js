140d69832ccac613c98701e5af9a738d5647b1a8
Ben Gorenstein <bengorenstein@gmail.com>
2021-01-14 18:26:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ function logWhisper(string){
 var uppercase = "HELLO"
 var lowercase = "hello"
 var p = "I love you, Grandma"
+
 function sayHiToGrandma(string){
-  if (string = lowercase) {
-    return 'I can\'t hear you!';
-}else if (string = uppercase){
-  return "YES INDEED!";
+  if (string === lowercase) {
+    return 'I can\'t hear you!'
+}else if (string === uppercase){
+  return "YES INDEED!"
 }else if (p){
-  return "I love you, too.";
+  return "I love you, too."
 }
 }
